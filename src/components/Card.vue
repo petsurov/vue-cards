ec9ebe0fc7edd8340622f9c29b06b43f89a97f1e
petsurov <dmitrypechurov04@gmail.com>
2021-06-13 22:11:45 +0300

    create card sides and flips

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,20 +1,62 @@
 <script>
 export default{
     props: {
+        position: {
+            type: Number,
+            required: true
+        },
         value: {
             type: Number,
             required: true
+        },
+        visible: {
+            type: Boolean,
+            default: false
+        }
+    },
+    setup(props,context) {
+        const selectCard = () => {
+            context.emit('select-card', {
+                position: props.position
+            })
+        }
+        return {
+            selectCard
         }
     }
 }
 </script>
 
 <template>
-    <div class="card">
-        {{ value }}
+    <div class="card" @click="selectCard">
+        <div v-if="visible" class="card-side front">
+            {{ value }}
+        </div>
+        <div v-else class="card-side back">
+            Back
+        </div>
     </div>
 </template>
 
 <style>
+.card{
+  border: 5px solid #ccc;
+  position: relative;
+}
 
+.card-side {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+}
+
+.card-side.front {
+    background: red;
+    color: white;
+}
+
+.card-side.back {
+    background: blue;
+    color: white;
+}
 </style>
