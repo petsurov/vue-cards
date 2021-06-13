379a980d5e7c93e0d62e53d9ffd9c88d90d45035
petsurov <dmitrypechurov04@gmail.com>
2021-06-13 18:39:43 +0300

    create game board

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <h1>Peek-a-Vue</h1>
+  <section class="game-board">
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.card{
+  border: 5px solid #ccc;
+}
+
+.game-board{
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-row-gap: 30px;
+  justify-content: center;
+}
+</style>
